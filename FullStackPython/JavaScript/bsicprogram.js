bc1287a76fab12d895675ff3464c262d0b3b7031
JeevanIT <jeevanitinfo@gmail.com>
2025-05-31 07:51:49 +0530

    first commit

---
+++ FullStackPython/JavaScript/bsicprogram.js
@@ -0,0 +1,11 @@
+function chnagebgcolor(){
+
+    /* document.body.style.backgroundColor = "lightblue"; */
+    document.bgColor = "lightblue";
+  /*   var colors = ["red", "blue", "green", "yellow", "purple", "orange"];
+    var randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;   */
+}
+function bgcolor2(){
+    document.bgColor = "lightgreen";
+}
